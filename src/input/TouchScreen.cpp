021260f3f7b2c373267729ed698b2f6310a111ba
IlikeChooros <pawel.smolnicki@gmail.com>
2022-11-05 08:10:19 +0100

    Working TouchScreen

--- src/input/TouchScreen.cpp
+++ src/input/TouchScreen.cpp
@@ -1,5 +1,10 @@
 #include "TouchScreen.h"
 
+void do_nothing()
+{
+    return;
+}
+
 TouchScreen::TouchScreen(TFT_eSPI* tft, uint16_t* data)
 {
     this->_tft = tft;
@@ -7,15 +12,17 @@ TouchScreen::TouchScreen(TFT_eSPI* tft, uint16_t* data)
     max_interval = 100;
     time_passed = 0;
     counter = 0;
+    checked = false;
+    check_on_relase = true;
 
 
     prev_x = 0;
     prev_y = 0;
-}
 
-void TouchScreen::do_nothing()
-{
-    return;
+    this->on_down(do_nothing);
+    this->on_left(do_nothing);
+    this->on_right(do_nothing);
+    this->on_up(do_nothing);
 }
 
 void TouchScreen::read()
@@ -32,19 +39,75 @@ void TouchScreen::read()
                 x_change += (x - prev_x)/2;
                 y_change += (y - prev_y)/2;
             }
-            Serial.println("X: "+String(x)+"  Y: "+String(y) + "   C: "+String(counter) + " Y_CH: "+String(y_change) + "  X_CH: "+String(x_change));
+            //Serial.println("X: "+String(x)+"  Y: "+String(y) + "   C: "+String(counter) + " Y_CH: "+String(y_change) + "  X_CH: "+String(x_change));
             counter++;
 
             prev_x = x;
             prev_y = y;
+
         }
-        else{
-            counter = 0;
-            y_change = 0;
-            x_change = 0;
+        if(!check_on_relase)
+        {
+            check_on_relase = true;
         }
         time_passed = millis();
     }
+
+    else if (check_on_relase && millis() - time_passed > max_interval)
+    {
+        if (counter > 5)
+        {
+            Serial.println("");
+            if (isHorizontal(x_change, y_change))
+            {
+                if (x_change<0)
+                {
+                    this->_on_right();
+                }
+                else{
+                    this->_on_left();
+                }
+            }
+
+            else if (isVertical(x_change, y_change))
+            {
+                if (y_change < 0)
+                {
+                    this->_on_down();
+                }
+                else{
+                    this->_on_up();
+                }
+            }
+            //Serial.println(" C: "+String(counter) + " Y_CH: "+String(y_change) + "  X_CH: "+String(x_change));
+            //Serial.println("");
+
+            check_on_relase = false;
+        }
+
+
+        counter = 0;
+        y_change = 0;
+        x_change = 0;
+    }
+}
+
+bool TouchScreen::isVertical(int16_t d_x, int16_t d_y)
+{
+    if (abs(d_x) < MIN_HORIZONTAL)
+    {
+        return abs(d_y) > VERTICAL_VAL;
+    }
+    return false;
+}
+
+bool TouchScreen::isHorizontal(int16_t d_x, int16_t d_y)
+{
+    if (abs(d_y) < MIN_VERTICAL)
+    {
+        return abs(d_x) > HORIZONTAL_VAL;
+    }
+    return false;
 }
 
 void TouchScreen::on_left(void(*left)(void))
--- src/input/TouchScreen.h
+++ src/input/TouchScreen.h
@@ -4,6 +4,12 @@
 #include <TFT_eSPI.h>
 #include <SPI.h>
 
+#define VERTICAL_VAL 25
+#define HORIZONTAL_VAL 25
+
+#define MIN_HORIZONTAL 25
+#define MIN_VERTICAL 25
+
 class TouchScreen
 {
     TFT_eSPI* _tft;
@@ -11,22 +17,24 @@ class TouchScreen
     uint8_t counter;
     uint16_t max_interval;
 
+    bool check_on_relase;
+    bool checked;
     int16_t x_change;
     int16_t y_change;
 
     int16_t prev_x;
     int16_t prev_y;
 
+    bool isVertical(int16_t d_x, int16_t d_y);
+    bool isHorizontal(int16_t d_x, int16_t d_y);
 
     void (*_on_left)();
     void (*_on_right)();
     void (*_on_up)();
     void (*_on_down)();
-    void do_nothing();
     public:
     TouchScreen(TFT_eSPI *tft ,uint16_t* touchData);
     void read();
-
     void on_left(void(*)(void));
     void on_right(void(*)(void));
     void on_up(void(*)(void));
